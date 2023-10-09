5e3c8c24e1d3eac0e0107f16bc1b05d5ce1a6e9c
Umar Afzal Khan <90247432+Umareee@users.noreply.github.com>
2023-10-09 20:28:30 +0500

    Time: 118 ms (27.38%), Space: 47.3 MB (54.38%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let roman = {'M' : 1000, 'CM':900, 'D':500,  'CD':400,   'C':100, 'XC':90, 'L':50,'XL':40,'X':10, 'IX':9,'V':5,'IV':4,'I':1 }
+    let result = 0;
+    for(let i = 0;i<s.length;i++)
+    {          
+        let curr = roman[s[i]];
+        let next = roman[s[i+1]];
+        if(curr < next)
+        {   
+            result += next-curr;
+            i++;
+        }
+        else{
+            result += curr;
+        }   
+    }
+    return result;         
+};            
