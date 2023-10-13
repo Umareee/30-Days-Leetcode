c1fca479e40cb008b18b5978a602151c3f79c818
Umar Afzal Khan <90247432+Umareee@users.noreply.github.com>
2023-10-13 11:21:35 +0500

    Time: 45 ms (93.82%), Space: 42.4 MB (50.92%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -3,19 +3,7 @@
  * @param {number} extraCandies
  * @return {boolean[]}
  */
-var kidsWithCandies = function(candies, extraCandies) {
-        let result = [];
-        let max = Math.max(...candies)
-        let n = candies.length
-        for(let i = 0;i < n; i++)
-        {
-            if(candies[i]+extraCandies >= max)
-            {
-                result.push(true)
-            }
-            else{
-                result.push(false)
-            }
-        }
-        return result
-};
+var kidsWithCandies = function(candies, extraCandies ,max = Math.max(...candies)) {
+
+   return  candies.map(candy => candy + extraCandies >= max)
+}
