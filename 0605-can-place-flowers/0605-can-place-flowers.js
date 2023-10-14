50d384f54a0ba55805d153effad0a56f290748fd
Umar Afzal Khan <90247432+Umareee@users.noreply.github.com>
2023-10-14 20:46:47 +0500

    Time: 41 ms (99.52%), Space: 44 MB (69.94%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let a = flowerbed.length;
+     
+    if(a == 1 && flowerbed[0] == 0 || n == 0)
+    {
+        return true
+    }
+    else if(a == 1 && flowerbed[0] == 1)
+    {
+    return false
+    }
+    if(flowerbed[0] == 0 && flowerbed[1] == 0)
+         {
+             n--;
+             flowerbed[0] = 1;
+         }
+     for(let i = 1 ; i < a - 1; i++ )
+     {
+         if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)
+         {
+             n--;
+             flowerbed[i] = 1;
+         }
+     }
+     if(flowerbed[a-1] == 0 && flowerbed[a-2] == 0)
+         {
+             n--;
+             flowerbed[a-1] = 1;
+         }
+    if(n <= 0)
+    {
+        return true;
+    }
+    else 
+    return false;
+};
