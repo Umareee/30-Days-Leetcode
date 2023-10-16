d56b1359cb78b48a1c131dc3094bcac774178a50
Umar Afzal Khan <90247432+Umareee@users.noreply.github.com>
2023-10-16 16:00:30 +0500

    Time: 78 ms (50.98%), Space: 46.6 MB (46.29%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let i = 0;
+    let j = 1;
+    while(j < nums.length)
+    {
+        if(nums[i] == 0 && nums[j] != 0)
+        {
+            [nums[i] , nums[j]] = [nums[j] , nums[i]]
+            i++;
+            j++;
+        }
+        else if(nums[i] == 0 && nums[j] == 0)
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    return nums;
+};
