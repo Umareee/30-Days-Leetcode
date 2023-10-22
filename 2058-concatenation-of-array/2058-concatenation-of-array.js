7a6266e76fc888ac93538bd42a287251be380a8f
Umar Afzal Khan <90247432+Umareee@users.noreply.github.com>
2023-10-22 21:57:00 +0500

    Time: 58 ms (89.09%), Space: 45.8 MB (35.98%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.js
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -3,14 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = [];
-    let j = 2;
-    // let i = 0;
-    let n = nums.length;
-    while(j != 0)
-    {
-        nums.forEach((i) => ans.push(i))
-        j--;
-    }
-    return ans
+    return [...nums,...nums]
 };
