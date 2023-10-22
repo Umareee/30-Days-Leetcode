be3024eb069353a4862e6e4de1e43a367e15cf20
Umar Afzal Khan <90247432+Umareee@users.noreply.github.com>
2023-10-22 19:23:03 +0500

    Time: 49 ms (92.46%), Space: 43 MB (85.61%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+   let unique = 0;
+   for(let i=0;i<nums.length;i++)
+   {
+       unique ^= nums[i];
+   } 
+   return unique;
+};
